4910802fb5cb6f06ce42cdd9bdc8f2c741dd3b11
minjoo <betaa0528@gmail.com>
2024-06-20 16:33:14 +0900

    [박민주] 6월 3주차

---
+++ PMJ1/boj/boj11004.py
@@ -0,0 +1,38 @@
+import sys
+
+def mergeSort(arr):
+    def sort(low, high):
+        if high - low < 2:
+            return
+        mid = (low + high) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        temp = []
+        l, h = low, mid
+
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < high:
+            temp.append(arr[h])
+            h += 1
+        for i in range(low, high):
+            arr[i] = temp[i - low]
+
+    return sort(0, len(arr))
+
+
+tc, target = map(int, sys.stdin.readline().strip().split())
+arr = list(map(int, sys.stdin.readline().strip().split()))
+mergeSort(arr)
+print(arr[target - 1])
