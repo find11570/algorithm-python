deb180ae1d5f8c9d1a1d17dfb483ffb0c988b97c
momodooly <momodooly@naver.com>
2024-06-27 18:22:49 +0900

    [모용] 6월 4주차

--- MY/boj/0604 Search/2023.py
+++ MY/boj/0604 Search/2023.py
@@ -44,19 +44,19 @@ def is_prime(num):
     return True
 
 def generate_primes(current, N):
-    if len(str(current)) == N:
+    if len(str(current)) == N:#길이가 맞으면 출력
         print(current)
         return
-    for digit in range(1, 10):  # Digits 1 through 9
-        new_number = current * 10 + digit
-        if is_prime(new_number):
-            generate_primes(new_number, N)
+    for digit in range(1, 10):  # Digits 1 through 9 #자릿수 추가해줌
+        new_number = current * 10 + digit #10곱하면 0이 붙으니 1의자리에 digit을 더해줌
+        if is_prime(new_number): #소수인지 판별검사
+            generate_primes(new_number, N) #소수면 다시 검사해서 자릿수검사하고 추가가주아~~
 
 N = int(input())
-if N == 1:
+if N == 1: #N이 1이면 그냥 소수로 날림
     primenum = [2, 3, 5, 7]
     for prime in primenum:
         print(prime)
 else:
-    for prime in [2, 3, 5, 7]:  # Start with single-digit primes
+    for prime in [2, 3, 5, 7]:  # 맨 앞첫번째 수부터 소수여야 하기 때문에 1의자리는 2,3,5,7로 시작
         generate_primes(prime, N)
