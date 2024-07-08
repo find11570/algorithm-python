5169ee48045454805f9d027816f098381ffd1621
momodooly <momodooly@naver.com>
2024-07-08 09:13:35 +0900

    [모용] 7월 1주차

--- MY/boj/0701 Greedy/1946.py
+++ MY/boj/0701 Greedy/1946.py
@@ -1,30 +1,32 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     N = int(input())
-    #지원자 성적 A,B성적순 나열 
-    #A,B성적에 따른 지원자 성적 정렬
-    scores = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: (x[0], x[1]))
+    # 지원자 성적 A,B성적순 나열
+    # A,B성적에 따른 지원자 성적 정렬
+    scores = sorted([list(map(int, input().split()))
+                    for _ in range(N)], key=lambda x: (x[0], x[1]))
 
     cnt = 0
-    A = B = N #지원자 최저등수
+    A = B = N  # 지원자 최저등수
     for a, b in scores:
-        if A > a or B > b: #바교할 지원자가 최저등수보다 한개라도 높으면
-            A = a #해당지원자의 성적을 비교군으로 바꿔줌.
+        if A > a or B > b:  # 바교할 지원자가 최저등수보다 한개라도 높으면
+            A = a  # 해당지원자의 성적을 비교군으로 바꿔줌.
             B = b
             cnt += 1
     print(cnt)
+
 ###################
-import sys
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     # 입력
     N = int(input())
     # 정렬(서류 기준 오름차순)
-    
-    applicant=sorted([list(map(int,input().split())) for _ in range(N)],key=lambda x:(x[0],x[1]))
+
+    applicant = sorted([list(map(int, input().split()))
+                       for _ in range(N)], key=lambda x: (x[0], x[1]))
     cut_line = applicant[0][1]  # 서류 1등의 면접 등수, 서류 등수로 이길 수 없으니 면접점수만 비교
 
     cnt = 1  # 합격자 수
@@ -35,5 +37,3 @@ for _ in range(T):
             cut_line = i[1]  # 면접 등수 컷라인이 높아짐. 서류로는 앞선 합격자보다 무조건 낮음 상대
             # 면접 등수로 이겨야 하기때문에 컷라인이 바뀜.
     print(cnt)
-
-
