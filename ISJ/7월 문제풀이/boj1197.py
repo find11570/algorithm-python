6f26de000709f8ff7f9fb8a85568e061a9724db7
isj0228 <166700353+isj0228@users.noreply.github.com>
2024-07-09 12:44:15 +0900

    7월 2주차
    최소 스패닝 트리

---
+++ ISJ/7월 문제풀이/boj1197.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n , v= map(int, input().split())
+
+INF = 9999999
+graph = [[] * (n+1) for _ in range(n+1)]
+
+for _ in range(v):
+    v1, v2, c = map(int, input().split())
+    graph[v1].append((c, v2))
+    graph[v2].append((c, v1))
+
+answer = 0
+visited = [False] * (n+1)
+min_heap = [(0,1)]
+
+while min_heap:
+    cost, node = heapq.heappop(min_heap)
+    if visited[node]:
+        continue
+    visited[node] = True
+    answer += cost
+
+    for neighbor,v in graph[node]:
+        if not visited[v]:
+            heapq.heappush(min_heap, (neighbor, v))
+
+print(answer)
