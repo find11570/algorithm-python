d5af215cb940df68d274d04c2d08343428ec3df4
momodooly <momodooly@naver.com>
2024-07-11 15:25:43 +0900

    [모용] 7월2주차

--- MY/boj/0702 DP/12852.py
+++ MY/boj/0702 DP/12852.py
@@ -22,5 +22,7 @@ for i in range(2,N+1): #2부터 N까지 1로 만들 수있는
         if len(dp[i//2])+1<len(dp[i]):
             dp[i]=dp[i//2]+[i]
 
+#본래 수가 포함된 길이 이므로 1을 빼줌
 print(len(dp[N])-1)
+#각 dp 는 1부터 N 까지의 경로를 담고 있으므로 역으로 출력
 print(*dp[N][::-1])
